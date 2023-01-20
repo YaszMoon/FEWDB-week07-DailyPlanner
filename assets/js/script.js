cbeab805e7fb6c78abacfd0c5790069dabc9c3cf
YaszMoon <yasminite@gmail.com>
2023-01-20 19:23:29 +0000

    Add pseudo code for row construction and applying past, present future colors

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+console.log('here')
+
+// Within the container
+// To create one line....
+// Create div class="row time-block" to hold line
+    // Create div for time class="hour col-2"
+    // Create div for description class="col-8"
+    // Create i for save button class="saveBtn col-2 fa-solid fa-floppy-disk"
+// For every row add data-time = 9AM etc....
+
+// Past present future
+// var timeNow
+// if hh in timeNow > dataset.time {setAttribute("class", "past")}
+// else if hh in timeNow < dataset.time {setAttribute("class", "future")}
+// else {setAttribute("class", present)}
