6c54abde511c3b72fc6b9765937a73b7eb748a3c
YaszMoon <yasminite@gmail.com>
2023-01-20 20:10:07 +0000

    Add code for one row

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,20 @@
 console.log('here')
 
 // Within the container
+var containerEl = $('.container')
 // To create one line....
 // Create div class="row time-block" to hold line
+var rowEl = $('<div>')
+rowEl.addClass('row time-block')
     // Create div for time class="hour col-2"
-    // Create div for description class="col-8"
+    var timeEl = $('<div class="hour col-2">')
+    // Create div for description class="description col-8"
+    var descripEl = $('<div class="description col-8">')
     // Create i for save button class="saveBtn col-2 fa-solid fa-floppy-disk"
+    var btnEl = $('<i class="saveBtn col-2 fa-solid fa-floppy-disk">')
+rowEl.append(timeEl, descripEl, btnEl)
+containerEl.append(rowEl)
+
 // For every row add data-time = 9AM etc....
 
 // Past present future
