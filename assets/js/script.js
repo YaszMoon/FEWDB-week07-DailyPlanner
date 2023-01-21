469fce0879a85b1a92c7fe99f9b54d23c6031763
YaszMoon <yasminite@gmail.com>
2023-01-21 00:50:10 +0000

    Add code to apply styling for past, present and future time slots

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,43 @@
 // Time variables
-var timeSlots = ['9AM', '10AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM']
-var today = moment()
+var timeSlots = ["9AM", "10AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+var today = moment();
 
-var currentDay = $('#currentDay')
-currentDay.text(today.format('dddd, MMMM YYYY'))
-
+// Text content for current day displayed at top of page
+var currentDay = $("#currentDay");
+currentDay.text(today.format("dddd, MMMM YYYY"));
 
 // Within the container
 var containerEl = $(".container");
 // To create one row
 function createRow(time) {
-// Create div class="row time-block" to hold line
-var rowEl = $("<div>");
-rowEl.addClass("row time-block");
-// Create div for time class="hour col-2"
-var timeEl = $('<div class="hour col-1">');
-timeEl.text(time)
-// Create div for description class="description col-8"
-var descripEl = $('<div class="description textarea col-10">');
-// Create i for save button class="saveBtn col-2 fa-solid fa-floppy-disk"
-var btnEl = $('<div class="saveBtn col-1"><i class="fas fa-save"></div>');
-rowEl.append(timeEl, descripEl, btnEl);
+  // Create div class="row time-block" to hold line
+  var rowEl = $("<div>");
+  rowEl.addClass("row time-block");
+  // Create div for time class="hour col-2"
+  var timeEl = $('<div class="hour col-1">');
+  timeEl.text(time);
+  // Create div for description class="description col-8"
+  var descripEl = $('<div class="description textarea col-10">');
 
-return rowEl
+  // Check time and add styling for past, present and future time slots
+  var timeCheck = moment(time, "hA");
+  if (today > timeCheck) {
+    descripEl.addClass("past");
+  } else if (today < timeCheck) {
+    descripEl.addClass("future");
+  } else {
+    descripEl.addClass("present");
+  }
+
+  // Create i for save button class="saveBtn col-2 fa-solid fa-floppy-disk"
+  var btnEl = $('<div class="saveBtn col-1"><i class="fas fa-save"></div>');
+  rowEl.append(timeEl, descripEl, btnEl);
+
+  return rowEl;
 }
 
-// Placeholder to check that function works
-var x = '9AM'
-
-for (var i=0; i<timeSlots.length; i++) {
-   var newRow = createRow(timeSlots[i])
-containerEl.append(newRow); 
+// Initialise Planner
+for (var i = 0; i < timeSlots.length; i++) {
+  var newRow = createRow(timeSlots[i]);
+  containerEl.append(newRow);
 }
-
-
-
-// For every row add data-time = 9AM etc....
-
-// Past present future
-// var timeNow
-// if hh in timeNow > dataset.time {setAttribute("class", "past")}
-// else if hh in timeNow < dataset.time {setAttribute("class", "future")}
-// else {setAttribute("class", present)}
