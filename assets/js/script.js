5c8ce0f141f308058ab8ec2a2733926677f6ad6f
YaszMoon <yasminite@gmail.com>
2023-01-21 00:21:33 +0000

    Add createRow(), time variables and #currentDay text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,37 @@
-console.log('here')
+// Time variables
+var timeSlots = ['9AM', '10AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM']
+var today = moment()
+
+var currentDay = $('#currentDay')
+currentDay.text(today.format('dddd, MMMM YYYY'))
+
 
 // Within the container
-var containerEl = $('.container')
-// To create one line....
+var containerEl = $(".container");
+// To create one row
+function createRow(time) {
 // Create div class="row time-block" to hold line
-var rowEl = $('<div>')
-rowEl.addClass('row time-block')
-    // Create div for time class="hour col-2"
-    var timeEl = $('<div class="hour col-2">')
-    // Create div for description class="description col-8"
-    var descripEl = $('<div class="description col-8">')
-    // Create i for save button class="saveBtn col-2 fa-solid fa-floppy-disk"
-    var btnEl = $('<i class="saveBtn col-2 fa-solid fa-floppy-disk">')
-rowEl.append(timeEl, descripEl, btnEl)
-containerEl.append(rowEl)
+var rowEl = $("<div>");
+rowEl.addClass("row time-block");
+// Create div for time class="hour col-2"
+var timeEl = $('<div class="hour col-1">');
+timeEl.text(time)
+// Create div for description class="description col-8"
+var descripEl = $('<div class="description textarea col-10" type="input">');
+// Create i for save button class="saveBtn col-2 fa-solid fa-floppy-disk"
+var btnEl = $('<div class="saveBtn col-1"><i class="fas fa-save"></div>');
+rowEl.append(timeEl, descripEl, btnEl);
+
+return rowEl
+}
+
+// Placeholder to check that function works
+var x = '9AM'
+
+
+
+var newRow = createRow(x)
+containerEl.append(newRow);
 
 // For every row add data-time = 9AM etc....
 
