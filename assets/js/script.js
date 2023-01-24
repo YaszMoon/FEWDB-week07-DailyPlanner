2e04a44554870783709b3ca217996e7f4777da76
YaszMoon <yasminite@gmail.com>
2023-01-24 13:52:21 +0000

    Set input value to previously save value

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,20 @@
 // Time variables
-var timeSlots = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+var timeSlots = [
+  "9AM",
+  "10AM",
+  "11AM",
+  "12PM",
+  "1PM",
+  "2PM",
+  "3PM",
+  "4PM",
+  "5PM",
+];
 var today = moment();
 
 // Text content for current day displayed at top of page
 var currentDay = $("#currentDay");
-currentDay.text(today.format("dddd, MMMM YYYY"));
+currentDay.text(today.format("dddd Do MMMM YYYY"));
 
 // Within the container
 var containerEl = $(".container");
@@ -16,23 +26,33 @@ function createRow(time) {
   // Create div for time class="hour col-2"
   var timeEl = $('<div class="hour col-1">');
   timeEl.text(time);
-  timeEl.css({"padding-top": "30px"})
+  timeEl.css({ "padding-top": "30px" });
+
   // Create div for description class="description col-8"
-  var descripEl = $(`<input type="text" class="description textarea col-10" data-timeslot=${time}>`);
+  var savedText = localStorage.getItem(time)
+  if (savedText == null) {
+    savedText = ""
+  }
+  var descripEl = $(
+    `<input type="text" class="description textarea col-10" id=${time}>`
+  );
+  descripEl.val(savedText)
 
   // Check time and add styling for past, present and future time slots
   var timeCheck = moment(time, "hA");
   if (today > timeCheck) {
     descripEl.addClass("past");
-  } else if (today < timeCheck && today > timeCheck.subtract(1, 'hour')) {
+  } else if (today < timeCheck && today > timeCheck.subtract(1, "hour")) {
     descripEl.addClass("present");
   } else {
     descripEl.addClass("future");
   }
 
   // Create i for save button class="saveBtn col-2 fa-solid fa-floppy-disk"
-  var btnEl = $(`<div class="saveBtn col-1"><i class="fas fa-save" data-id=${time}></div>`);
-  btnEl.css({"padding-top": "30px"})
+  var btnEl = $(
+    `<div class="saveBtn col-1"><i class="fas fa-save" data-id=${time}></div>`
+  );
+  btnEl.css({ "padding-top": "30px" });
 
   rowEl.append(timeEl, descripEl, btnEl);
 
@@ -45,16 +65,8 @@ for (var i = 0; i < timeSlots.length; i++) {
   containerEl.append(newRow);
 }
 
-$('.saveBtn i').on('click', function(event) {
-    console.log('here')
-  
-  // var test1 = event.target.dataset.id
-  //   var test2 = $(`[data-timeslot=${test1}]`)
-
-  //   if (test1 == '9AM') {
-  //     console.log(test2.value)
-  //   }
-      
-    
-
-  })
+$(".saveBtn i").on("click", function (event) {
+  var check = event.target.dataset.id;
+  var eventInput = $('#'+check).val();
+  localStorage.setItem(check, eventInput)
+});
